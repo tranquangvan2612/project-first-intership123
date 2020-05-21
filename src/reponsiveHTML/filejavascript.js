e4ea04ca3f4736beb9af68650dec731ae8bf3aa4
tranquangvan2612 <vantran.24hdev@gmail.com>
2020-05-21 14:01:21 +0700

    complete htmlcss

--- src/reponsiveHTML/filejavascript.js
+++ src/reponsiveHTML/filejavascript.js
@@ -1,7 +1,7 @@
 function consoleForm(e) {
   e.preventDefault();
   console.log(e);
-  
+
   let inputName = document.getElementById("cardName").value;
   if (!inputName) {
     document.getElementById("messageErrorName").innerHTML = "Error empty value";
@@ -31,4 +31,47 @@ function consoleForm(e) {
     document.getElementById("messageSuccess").innerHTML =
       "Login in successfully";
   }
+}
+// var slide_index = 1;
+// displaySlides(slide_index);
+
+// function nextSlide(n) {
+//     displaySlides(slide_index += n);
+// }
+
+// function currentSlide(n) {
+//     displaySlides(slide_index = n);
+// }
+// function displaySlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("carousel-item");
+//   console.log(slides);
+//   console.log("slides  " + slides +slides.length);
+//   if (n > slides.length) { slide_index = 1 }
+//   if (n < 1) { slide_index = slides.length }
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   slides[slide_index - 1].style.display = "block";
+// }
+let slide_index = 1;
+displaySlide(slide_index);
+function nextSlide(n) {
+  displaySlide((slide_index += n));
+}
+function displaySlide(n) {
+  let i = 0;
+  let slides = document.getElementsByClassName("carousel-item");
+  console.log("slide_index " + slide_index);
+  console.log("length:" + slides.length);
+  if (n > slides.length) {
+    slide_index = 1;
+  }
+  if (n < 1) {
+    slide_index = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slide_index - 1].style.display = "block";
 }
