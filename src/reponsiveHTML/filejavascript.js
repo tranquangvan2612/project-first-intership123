902c3ef79cd8897b3707f32516461423258a383b
tranquangvan2612 <vantran.24hdev@gmail.com>
2020-05-25 08:26:13 +0700

    update carousel slide

--- src/reponsiveHTML/filejavascript.js
+++ src/reponsiveHTML/filejavascript.js
@@ -32,46 +32,72 @@ function consoleForm(e) {
       "Login in successfully";
   }
 }
-// var slide_index = 1;
-// displaySlides(slide_index);
 
-// function nextSlide(n) {
-//     displaySlides(slide_index += n);
-// }
+// let slide_index = 1;
+// displaySlide(slide_index);
+// function nextSlide(index_slide_next) {
+//   console.log("left");
 
-// function currentSlide(n) {
-//     displaySlides(slide_index = n);
+//   let slides_next = document.getElementsByClassName("slide-update");
+//   for (let i = 0; i < slides_next.length; i++) {
+//     slides_next[i].style.animationName = "show_slide-left";
+//   }
+
+//   displaySlide((slide_index += index_slide_next));
 // }
-// function displaySlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("carousel-item");
-//   console.log(slides);
-//   console.log("slides  " + slides +slides.length);
-//   if (n > slides.length) { slide_index = 1 }
-//   if (n < 1) { slide_index = slides.length }
+// function prevSlide(index_slide_next) {
+//   console.log("right");
+//   let slides_prev = document.getElementsByClassName("slide-update");
+
+//   for (let i = 0; i < slides_prev.length; i++) {
+//     slides_prev[i].style.animationName = "show_slide_right";
+//   }
+
+//   displaySlide((slide_index += index_slide_next));
+// }
+// function displaySlide(index_slide_next) {
+//   let i = 0;
+//   let slides = document.getElementsByClassName("carousel-item");
+//   console.log("slide_index " + slide_index);
+//   console.log("length:" + slides.length);
+//   if (index_slide_next > slides.length) {
+//     slide_index = 1;
+//   }
+//   if (index_slide_next < 1) {
+//     slide_index = slides.length;
+//   }
 //   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
+//     slides[i].style.display = "none";
 //   }
 //   slides[slide_index - 1].style.display = "block";
 // }
-let slide_index = 1;
-displaySlide(slide_index);
-function nextSlide(n) {
-  displaySlide((slide_index += n));
+
+let slides_container = document.getElementById("sharing-carousel");
+let slides = document.querySelectorAll(".carousel-item");
+
+function prevSlide() {
+  let element = document.querySelector(".carousel-item");
+  document.getElementById("sharing-carousel").style.transitionDuration = "1.5s";
+  document.getElementById("sharing-carousel").style.left = "-100%";
+  setTimeout(function () {
+    slides_container.removeChild(element);
+    slides_container.appendChild(element);
+    document.getElementById("sharing-carousel").style.transitionDuration = "0s";
+    document.getElementById("sharing-carousel").style.left = "0%";
+  }, 1500);
 }
-function displaySlide(n) {
-  let i = 0;
-  let slides = document.getElementsByClassName("carousel-item");
-  console.log("slide_index " + slide_index);
-  console.log("length:" + slides.length);
-  if (n > slides.length) {
-    slide_index = 1;
-  }
-  if (n < 1) {
-    slide_index = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slide_index - 1].style.display = "block";
+
+function nextSlide() {
+  console.log("asdas");
+  const elCarousel = document.getElementById("sharing-carousel");
+
+  let element = document.querySelector(".carousel-item");
+  slides_container.appendChild(element);
+
+  elCarousel.style.transitionDuration = "0s";
+  elCarousel.style.left = "-100%";
+  setTimeout(function () {
+    elCarousel.style.transitionDuration = "1.5s";
+    elCarousel.style.left = "0%";
+  }, 10);
 }
